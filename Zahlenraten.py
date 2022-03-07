be35fb80e2e424270b541c5f6bf3c556bb7f4a74
70H4NN2S <57498980+70H4NN2S@users.noreply.github.com>
2022-03-07 18:37:47 +0100

    Änderung
    Ist eine kleine Verbesserung. Schau sie dir an

--- Zahlenraten.py
+++ Zahlenraten.py
@@ -1,73 +1,43 @@
-print("Herzlich willkommen beim Zahlen erraten, rate eine Zahl von 1 bis 50:  ")
 from random import randint
-ratezahl=randint(1,50)
 
-zahl=int(input("Zahl:  "))
+print("Herzlich willkommen beim Zahlenraten")
 
+#Richtige angabe der Zahlen wird überprüft
+while True:
+	min = int(input("Gib die kleinste Zahl an: "))
+	max = int(input("Gib die größte Zahl ein: "))
+	if max <= min:
+		print("Die erste Zahl muss kleiner sein")
+	else:
+		break
 
-if zahl < ratezahl:
-	print("zu klein!")
-	
-if zahl> ratezahl:
-	print("zu groß!")
-if zahl==ratezahl:
-	print("super du hast gewonnen!")
-	print("1Versuch")
-	input()
-	exit()
-zahl=int(input("Zahl:  "))
-if zahl==ratezahl:
-	print("super du hast gewonnen!")
-	print("2Versuche")
-	input()
-	exit()
+#Variabeln werden deklariert
+versuche = 0
+ergebnis = randint(min,max)
+zahlen = []
 
-if zahl < ratezahl:
-	print("zu klein!")
-	
-if zahl> ratezahl:
-	print("zu groß!")
-zahl=int(input("Zahl:  "))
-if zahl==ratezahl:
-	print("super du hast gewonnen!")
-	print("3 versuche")
-	input()
-	exit()
-if zahl < ratezahl:
-	print("zu klein!")
-	
-if zahl> ratezahl:
-	print("zu groß!")
-zahl=int(input("Zahl:  "))
-if zahl==ratezahl:
-	print("super du hast gewonnen!")
-	print("4 Versuche")
-	input()
-	exit()
-if zahl < ratezahl:
-	print("zu klein!")
-	
-if zahl> ratezahl:
-	print("zu groß!")
-zahl=int(input("Zahl:  "))
-if zahl==ratezahl:
-	print("super du hast gewonnen!")
-	print("5 versuche")
-	input()
-	exit()
-if zahl < ratezahl:
-	print("zu klein!")
-	
-if zahl> ratezahl:
-	print("zu groß!")
-zahl=int(input("Zahl:  "))
-if zahl==ratezahl:
-	print("super du hast gewonnen!")
-	print("6 versuche")
-	input()
-	exit()
-print("Du hast verloren")
-print("Die Zahl war: ")
-print(ratezahl)
-print("created by RLN")
-input(exit)
+#Das Spiel läuft
+while True:
+	zahl = int(input("Rate eine Zahl: "))
+	#Eingegebene Zahl wird validiert
+	if zahl < min or zahl > max:
+		print(f"Die eingegebene Zahl ist zu groß/klein. Es muss eine Zahl zwischen {min} und {max} sein")
+		continue
+	if zahl in zahlen:
+		print("Du hast es mit dießer Zahl schon einmal ausprobiert. Versuche es mit einer anderen Zahl")
+		continue
+	#Eingegebene Zahl wird verarbeitet
+	versuche += 1
+	zahlen.append(zahl)
+
+	#Eingegebene Zahl wird verglichen
+	if zahl < ergebnis:
+		print("Die Zahl ist zu klein")
+	elif zahl > ergebnis:
+		print("Die Zahl ist zu groß")
+	else:
+		print(f"Du hast mit {versuche} Versuch(en) die richtige Zahl {zahl} erraten.")
+		break
+		
+input()
+exit
